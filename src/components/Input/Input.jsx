a44dda78ce1804929a34da811cab6f7055677d81
Akbar <akbarjonjorayev001@gmail.com>
2024-05-19 16:44:35 +0500

    password Input & ref

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,32 +1,54 @@
-import { useState } from 'react'
+import { useState, forwardRef } from 'react'
+
+import Button from '../Button/Button'
 
 import './Input.css'
 
-export default function Input({
-  type = 'text',
-  label: inputLabel,
-  value: iValue,
-  ...props
-}) {
-  const [value, setValue] = useState(iValue === undefined ? '' : iValue)
+const Input = forwardRef(
+  ({ type = 'text', label: inputLabel, value: iValue, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [value, setValue] = useState(iValue === undefined ? '' : iValue)
 
-  function changeInput(e) {
-    setValue(e.target.value)
-    if (props.onChange) props.onChange(e)
+    function changeInput(e) {
+      setValue(e.target.value)
+      if (props.onChange) props.onChange(e)
+    }
+
+    return (
+      <>
+        <div className="input_area">
+          <label htmlFor={inputLabel}>{inputLabel}</label>
+          <div className="list_x">
+            <input
+              ref={ref}
+              value={value}
+              id={inputLabel}
+              type={
+                type === 'password'
+                  ? showPassword
+                    ? 'text'
+                    : 'password'
+                  : type
+              }
+              onChange={changeInput}
+              {...props}
+            />
+            {type === 'password' && (
+              <Button
+                type="button"
+                className="d_f_ce"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <span className="material-symbols-outlined">
+                  visibility{showPassword ? '_off' : ''}
+                </span>
+              </Button>
+            )}
+          </div>
+        </div>
+      </>
+    )
   }
+)
 
-  return (
-    <>
-      <div className="input_area">
-        <label htmlFor={inputLabel}>{inputLabel}</label>
-        <input
-          value={value}
-          id={inputLabel}
-          type={type}
-          onChange={changeInput}
-          {...props}
-        />
-      </div>
-    </>
-  )
-}
+export default Input
