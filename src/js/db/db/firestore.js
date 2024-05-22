8b66739c8de021365162260fdf300daa104e472b
Akbar <akbarjonjorayev001@gmail.com>
2024-05-22 16:08:09 +0500

    firebase functions

--- src/js/db/db/firestore.js
+++ src/js/db/db/firestore.js
@@ -1,13 +1,65 @@
-import { collection, setDoc, doc } from 'firebase/firestore'
+import {
+  collection,
+  setDoc,
+  doc,
+  getDoc,
+  updateDoc,
+  increment,
+} from 'firebase/firestore'
 import { firestoreDB } from './firebaseDB'
 
-export function saveFirestore(collectionName, docName, data) {
+export async function saveFirestore(collectionName, docName, data) {
   const dataRef = collection(firestoreDB, collectionName)
-  return setDoc(doc(dataRef, docName), data)
-    .then(() => {
+
+  try {
+    await setDoc(doc(dataRef, docName), data)
+    return true
+  } catch {
+    return false
+  }
+}
+
+export async function loadFromFirestore(collectionName, docName) {
+  try {
+    const docRef = doc(firestoreDB, collectionName, docName)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) return docSnap.data()
+    return false
+  } catch (error) {
+    return false
+  }
+}
+
+export async function editFirestore(collectionName, docName, newData) {
+  try {
+    const docRef = doc(firestoreDB, collectionName, docName)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+      await setDoc(docRef, newData, { merge: true })
       return true
+    }
+    return false
+  } catch (error) {
+    return false
+  }
+}
+
+export async function incrementField(
+  collectionName,
+  docName,
+  fieldName,
+  incrementBy
+) {
+  const docRef = doc(firestoreDB, collectionName, docName)
+
+  try {
+    await updateDoc(docRef, {
+      [fieldName]: increment(incrementBy),
     })
-    .catch(() => {
-      return false
-    })
+    return true
+  } catch (err) {
+    return false
+  }
 }
