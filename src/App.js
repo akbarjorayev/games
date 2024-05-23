40c8773cd4efcad8f88358eb8e67afeec8f37f8f
Akbar <akbarjonjorayev001@gmail.com>
2024-05-23 18:48:08 +0500

    check localstorage in every load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { checkRunApp } from './js/utils/checker'
+
 import './css/App.css'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -8,6 +10,8 @@ import { Signup } from './pages/Account/Signup/Signup'
 const Login = React.lazy(() => import('./pages/Account/Login/Login'))
 
 export default function App() {
+  checkRunApp()
+
   return (
     <>
       <BrowserRouter>
