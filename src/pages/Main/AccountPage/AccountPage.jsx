886597f2c3a2556eea73e0a2bafe01871107753c
Akbar <akbarjonjorayev001@gmail.com>
2024-05-26 18:54:20 +0500

    initial view of Account Page & Avatar

--- src/pages/Main/AccountPage/AccountPage.jsx
+++ src/pages/Main/AccountPage/AccountPage.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react'
+
+import Avatar from '../../../components/Avatar/Avatar'
+
+import { loadFromLocalStorage } from '../../../js/db/local/localStorage'
+import { loadFromFirestore } from '../../../js/db/db/firestore'
+
 import './AccountPage.css'
 
 export default function AccountPage() {
+  const [account, setAccount] = useState(false)
+
+  useEffect(() => {
+    const id = loadFromLocalStorage('games').accounts.active
+    async function loadData() {
+      const data = await loadFromFirestore('accounts', id)
+      setAccount(data)
+    }
+    loadData()
+  }, [])
+
+  if (!account)
+    return (
+      <>
+        <div className="d_f_jc_ce">'Account Loading...'</div>
+      </>
+    )
+
   return (
     <>
-      <div>AccountPage</div>
+      <div className="d_f_jc_ce">
+        <Avatar
+          letter={account.name[0]}
+          style={{ fontSize: '50px', width: '100px' }}
+        />
+      </div>
     </>
   )
 }
