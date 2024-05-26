3780a929886a7c0ac1003b32fcaa065c94fa8d18
Akbar <akbarjonjorayev001@gmail.com>
2024-05-26 11:48:14 +0500

    usage of GameArea in App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 import { Signup } from './pages/Account/Signup/Signup'
 const Login = React.lazy(() => import('./pages/Account/Login/Login'))
+const GameArea = React.lazy(() => import('./pages/Main/GameArea/GameArea'))
 
 export default function App() {
   checkRunApp()
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/account/signup/phone" element={Signup.phone} />
           <Route path="/account/signup/userdata" element={Signup.userData} />
           <Route path="/account/login" element={<Login />} />
+          <Route path="/games" element={<GameArea />} />
         </Routes>
       </BrowserRouter>
     </>
