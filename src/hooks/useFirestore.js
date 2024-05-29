1e3a09746b0dab02cf399776cb76829688935dbf
Akbar <akbarjonjorayev001@gmail.com>
2024-05-29 16:37:34 +0500

    new useFirestore hook

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { loadFromFirestore } from '../js/db/db/firestore'
+
+export const useFirestore = (collectionName, docName, ...deps) => {
+  const [firestoreData, setFirestoreData] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await loadFromFirestore(collectionName, docName)
+      setFirestoreData(data ? data : false)
+    }
+
+    loadData()
+  }, [collectionName, docName, ...deps])
+
+  return [firestoreData, setFirestoreData]
+}
+
+export const useFirestoreAll = (collectionName, docNames, ...deps) => {
+  const [firestoreData, setFirestoreData] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await Promise.all(
+        docNames.map(async (docName) => {
+          return await loadFromFirestore(collectionName, docName)
+        })
+      )
+      setFirestoreData(data ? data : false)
+    }
+
+    loadData()
+  }, [collectionName, docNames, ...deps])
+
+  return [firestoreData, setFirestoreData]
+}
