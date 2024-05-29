8f3ca8a00a2a98ce5296fc78e5ef94044ba756dc
Akbar <akbarjonjorayev001@gmail.com>
2024-05-29 17:50:11 +0500

    status fixed

--- src/status/status.util.js
+++ src/status/status.util.js
@@ -1,6 +1,6 @@
 import { loadFromLocalStorage } from '../js/db/local/localStorage'
 
-const maxAccountLimit = 3
+export const maxAccountLimit = 3
 
 export function getAccountIsAtLimit() {
   const accounts = loadFromLocalStorage('games')?.accounts.ids
