8c6f5a4faa7dd5fee919653ffbd681d8aaa142aa
Akbar <akbarjonjorayev001@gmail.com>
2024-06-01 19:34:18 +0500

    AccountPageFollow fixed

--- src/pages/Main/AccountPage/components/AccountPageFollow.jsx
+++ src/pages/Main/AccountPage/components/AccountPageFollow.jsx
@@ -1,10 +1,10 @@
 export default function AccountPageFollow() {
   return (
     <div className="list_x">
-      <div className="con_bg_none blur_theme_bg blur_ha pd_tb_small scale_trns cur_pointer">
+      <div className="con blur_theme_bg blur_ha pd_tb_small scale_trns cur_pointer">
         0 follwers
       </div>
-      <div className="con_bg_none blur_theme_bg blur_ha pd_tb_small scale_trns cur_pointer">
+      <div className="con blur_theme_bg blur_ha pd_tb_small scale_trns cur_pointer">
         0 follwing
       </div>
     </div>
