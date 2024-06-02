34ca4b655cb9c71b78993afebc092ace263593ee
Akbar <akbarjonjorayev001@gmail.com>
2024-06-02 13:42:09 +0500

    Game Page improved

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import { Signup } from './pages/SignupLoginPage/Signup/Signup'
 const Login = React.lazy(() => import('./pages/SignupLoginPage/Login/Login'))
 const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'))
-const GamesList = React.lazy(() => import('./pages/GamesPage/GameList'))
+const GamePage = React.lazy(() => import('./pages/GamesPage/GamePage'))
 const SearchPage = React.lazy(() => import('./pages/SearchPage/SearchPage'))
 
 export default function App() {
@@ -23,7 +23,7 @@ export default function App() {
           <Route path="/account/signup/phone" element={Signup.phone} />
           <Route path="/account/signup/user-data" element={Signup.userData} />
           <Route path="/account/login" element={<Login />} />
-          <Route path="/games" element={<GamesList />} />
+          <Route path="/games" element={<GamePage />} />
           <Route path="/search" element={<SearchPage />} />
         </Routes>
       </BrowserRouter>
