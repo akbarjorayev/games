31370b4b29566ecd7c63b77acc61a531e3604afa
Akbar <akbarjonjorayev001@gmail.com>
2024-06-02 14:38:57 +0500

    home page improved

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,13 +8,15 @@ export default function HomePage() {
   return (
     <>
       <div className="pos_full_page home_page d_f_ce list_y">
-        <div>
+        <div className="home_page_logo_con">
           <img src={logo} alt="Logo" />
-          <div
-            className="con home_page_play_btn fz_medium scale_trns cur_pointer"
-            onClick={() => goToHref('/games')}
-          >
-            Play
+          <div className="d_f_ce">
+            <div
+              className="con home_page_play_btn fz_medium scale_trns cur_pointer"
+              onClick={() => goToHref('/games')}
+            >
+              Play
+            </div>
           </div>
         </div>
       </div>
