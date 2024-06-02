d4f845c06ec59231670cb048f911b70acd6569d1
Akbar <akbarjonjorayev001@gmail.com>
2024-06-02 11:23:59 +0500

    new -> SearchWillbeFixed

--- src/pages/Main/Search/components/SearchStatus.jsx
+++ src/pages/Main/Search/components/SearchStatus.jsx
@@ -1,11 +1,18 @@
+import SearchWillbeFixed from './SearchWillbeFixed'
+
 import { SEARCH_PAGE_STATUS } from '../SearchPageStatus'
 
 export default function SearchStatus({ status }) {
   return (
     <>
-      <div className="h_100 d_f_ce list_y">
+      <div className="h_100 d_f_ce pos_r list_y search_status">
         <span className="material-symbols-outlined fz_big_icon">search</span>
-        {status === SEARCH_PAGE_STATUS.home && <div>Search for friends</div>}
+        {status === SEARCH_PAGE_STATUS.home && (
+          <>
+            <div>Search for friends</div>
+            <SearchWillbeFixed />
+          </>
+        )}
         {status === SEARCH_PAGE_STATUS.searching && (
           <div>Searching for friends</div>
         )}
