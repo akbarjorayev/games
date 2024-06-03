55dfd73bad89df1cf7ca00761850cb5a69660858
Akbar <akbarjonjorayev001@gmail.com>
2024-06-03 16:29:19 +0500

    text fixed

--- src/pages/SearchPage/components/SearchWillbeFixed.jsx
+++ src/pages/SearchPage/components/SearchWillbeFixed.jsx
@@ -5,7 +5,7 @@ export default function SearchWillbeFixed() {
     <>
       <div className="con search_fix_con w_100 fz_small pd_tb_small">
         <div className="txt_opa">
-          <div>Please enter your friend's full username.</div>
+          <div>Please enter your friend's full username or id.</div>
           <div>We're working on improving our search feature soon.</div>
         </div>
       </div>
