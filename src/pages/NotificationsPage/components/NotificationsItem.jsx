a41a6de4777549d283f36383c3b08e9f9b18c60b
Akbar <akbarjonjorayev001@gmail.com>
2024-06-03 16:05:58 +0500

    deny & accept buttons in notifications

--- src/pages/NotificationsPage/components/NotificationsItem.jsx
+++ src/pages/NotificationsPage/components/NotificationsItem.jsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 
+import Button from '../../../components/Button/Button'
+
 import { NotificationsContext } from '../NotificationsContext'
 import { removeNotification } from '../../../modules/notifications.module'
+import { NOTIFICATIONS_TYPES } from '../data/notificationsData'
 
 export default function NotificationsItem({ data }) {
   const { notifications, setNotifications } = useContext(NotificationsContext)
@@ -28,9 +31,10 @@ export default function NotificationsItem({ data }) {
   return (
     <>
       <div
-        className="con blur_theme_bg notification_item"
+        className="con blur_theme_bg notification_item list_y"
         onWheel={swipe}
         removed={remove ? 'removed' : ''}
+        new={data.new ? 'new' : ''}
       >
         <div className="d_f_jc_sb">
           <div>
@@ -41,6 +45,16 @@ export default function NotificationsItem({ data }) {
             {date.current.getHours()}:{date.current.getMinutes()}
           </div>
         </div>
+        {(data.type === NOTIFICATIONS_TYPES.frReqs ||
+          data.type === NOTIFICATIONS_TYPES.playReqs) && (
+          <>
+            <div className="line_x"></div>
+            <div className="list_x w_100_child">
+              <Button className="txt_red">Deny</Button>
+              <Button className="btn_cl">Accept</Button>
+            </div>
+          </>
+        )}
       </div>
     </>
   )
