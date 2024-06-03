1c51d6d1e0171fe5f7f0146612432ee901815c7b
Akbar <akbarjonjorayev001@gmail.com>
2024-06-03 13:12:34 +0500

    notification module fixed & send notification

--- src/modules/notifications.module.js
+++ src/modules/notifications.module.js
@@ -1,14 +1,38 @@
-import { editFirestore, incrementField } from '../js/db/db/firestore'
+import {
+  addToArrayFirestore,
+  editFirestore,
+  incrementField,
+  loadFromFirestore,
+} from '../js/db/db/firestore'
+import { loadFromLocalStorage } from '../js/db/local/localStorage'
 
-const lId = window.location.pathname.split('users/')[1]
+const lId = loadFromLocalStorage('games').accounts.active
 
-export async function removeNotification(id, ns) {
+export async function removeNotification(id) {
+  const ns = await loadFromFirestore('notifications', `${lId}`)
   const newNs = ns.notifications.filter((n) => n.id !== id)
 
-  await editFirestore('notifications', `${lId}`, {
-    notifications: newNs,
-  })
+  await editFirestore('notifications', `${lId}`, { notifications: newNs })
   await incrementField('notifications', `${lId}`, 'amount', -1)
 
   return newNs
+}
+
+export async function sendNotification(id, ns) {
+  const { amount } = await loadFromFirestore('notifications', `${id}`)
+
+  const notification = {
+    ...ns,
+    new: true,
+    id: amount + 1,
+    date: new Date().getTime(),
+  }
+
+  await addToArrayFirestore(
+    'notifications',
+    `${id}`,
+    'notifications',
+    notification
+  )
+  await incrementField('notifications', `${id}`, 'amount', 1)
 }
