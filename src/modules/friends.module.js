bd8e47afe608fd2245ed8f5264eb756725af1c98
Akbar <akbarjonjorayev001@gmail.com>
2024-06-05 07:35:31 +0500

    friends module improved

--- src/modules/friends.module.js
+++ src/modules/friends.module.js
@@ -19,7 +19,7 @@ export async function removeFollowing(userID, followingID) {
   const { following } = await loadFromFirestore('friends', `${userID}`)
   if (following?.length === 0) return
 
-  const newFollowing = following.filter((fID) => `${fID}` !== `${followingID}`)
+  const newFollowing = following?.filter((fID) => `${fID}` !== `${followingID}`)
   await editFirestore('friends', `${userID}`, { following: newFollowing })
   await incrementField('friends', `${followingID}`, 'followersAmount', -1)
 }
@@ -38,7 +38,7 @@ export async function removeFollowers(userID, followersID) {
   const { followers } = await loadFromFirestore('friends', `${userID}`)
   if (followers?.length === 0) return
 
-  const newFollowers = followers.filter((fID) => `${fID}` !== `${followersID}`)
+  const newFollowers = followers?.filter((fID) => `${fID}` !== `${followersID}`)
   await editFirestore('friends', `${userID}`, { followers: newFollowers })
   await incrementField('friends', `${followersID}`, 'followingAmount', -1)
 }
