5a770a436403a924fe0ece823a9253a824cb2b97
Akbar <akbarjonjorayev001@gmail.com>
2024-06-07 12:21:30 +0500

    game module functions

---
+++ src/modules/game.module.js
@@ -0,0 +1,30 @@
+import {
+  deleteFromRealtimeDB,
+  editToRealtimeDB,
+} from '../js/db/db/firebaseRealtime'
+import { saveToSession } from '../js/db/local/sessionStorage'
+import {
+  deleteGuestNotification,
+  getGameToken,
+  prepareReltimeDBForGame,
+  sendPlayReqNotification,
+} from './utils/game.module.util'
+
+export async function prepareGame(friendID, gameLink) {
+  const gameToken = await getGameToken()
+  saveToSession('gameToken', gameToken)
+  saveToSession('gameLink', gameLink)
+
+  await sendPlayReqNotification(friendID, gameLink, gameToken)
+  await prepareReltimeDBForGame(friendID, gameToken)
+}
+
+export async function endGame(gameToken) {
+  await deleteGuestNotification(gameToken)
+  await deleteFromRealtimeDB(`games/playing/${gameToken}`)
+}
+
+export async function acceptGame(gameToken) {
+  await deleteGuestNotification(gameToken)
+  await editToRealtimeDB(`games/playing/${gameToken}`, { playing: true })
+}
