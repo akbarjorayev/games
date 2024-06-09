e2d31bf49d0779a273543c9862dcce77d9d9ac63
Akbar <akbarjonjorayev001@gmail.com>
2024-06-09 11:24:06 +0500

    new behaviour for home page button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,22 @@
+import { useRef } from 'react'
+
 import logo from '../../imgs/logo/appLogo.webp'
 
 import { goToHref } from '../../js/utils/href'
+import { loadFromLocalStorage } from '../../js/db/local/localStorage'
 
 import './HomePage.css'
 
 export default function HomePage() {
+  const hasAccount = useRef(
+    loadFromLocalStorage('games')?.accounts.active
+  ).current
+
+  function changeHref() {
+    if (hasAccount) goToHref('/games')
+    if (!hasAccount) goToHref('/account/signup/phone')
+  }
+
   return (
     <>
       <div className="pos_full_page home_page d_f_ce list_y">
@@ -13,9 +25,10 @@ export default function HomePage() {
           <div className="d_f_ce">
             <button
               className="con btn_cl home_page_play_btn fz_medium scale_trns cur_pointer"
-              onClick={() => goToHref('/games')}
+              onClick={changeHref}
             >
-              Play
+              {hasAccount && <span>Play</span>}
+              {!hasAccount && <span>Create account</span>}
             </button>
           </div>
         </div>
