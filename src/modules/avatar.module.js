92656891238c02fb9f57974b9dd4d73f5b12709a
Akbar <akbarjonjorayev001@gmail.com>
2024-06-15 10:44:49 +0500

    downloadAvatar algorithm

--- src/modules/avatar.module.js
+++ src/modules/avatar.module.js
@@ -1,8 +1,17 @@
-import { uploadBytesToStore } from '../js/db/db/firebaseStore'
+import {
+  downloadFromStore,
+  uploadBytesToStore,
+} from '../js/db/db/firebaseStore'
 import { loadFromLocalStorage } from '../js/db/local/localStorage'
 
 export async function uploadAvatar(file) {
   const id = loadFromLocalStorage('games').accounts.active
   const uploaded = await uploadBytesToStore(`users/${id}/avatar`, file)
   return uploaded
+}
+
+export async function downloadAvatar(id) {
+  const avatarID = id || loadFromLocalStorage('games').accounts.active
+  const avatar = await downloadFromStore(`users/${avatarID}/avatar`)
+  return avatar
 }
