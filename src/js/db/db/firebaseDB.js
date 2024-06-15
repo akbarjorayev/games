f531f0415669d75ccf040773878a8023aea3c260
Akbar <akbarjonjorayev001@gmail.com>
2024-06-15 08:43:30 +0500

    new getStorage()

--- src/js/db/db/firebaseDB.js
+++ src/js/db/db/firebaseDB.js
@@ -3,9 +3,11 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const app = initializeApp(API.firebase)
 export const firestoreDB = getFirestore(app)
 export const firebaseRealtimeDB = getDatabase()
+export const firebaseStorage = getStorage()
 export const auth = getAuth(app)
 auth.languageCode = auth.useDeviceLanguage()
