6680c20e987779d1f419099d15a8470a31d7b1df
Akbar <akbarjonjorayev001@gmail.com>
2024-06-15 10:34:30 +0500

    new -> AvatarEditAlert

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,11 +1,45 @@
+import { useState } from 'react'
+
+import AvatarEditAlert from './AvatarEditAlert'
+
 import './Avatar.css'
 
-export default function Avatar({ style, letter }) {
+export default function Avatar({ style, letter, img, editable = false }) {
+  const [edit, setEdit] = useState(false)
+
   return (
     <>
-      <div className="avatar" style={style}>
-        <div className="avatar_letter">{letter}</div>
+      <div
+        className={`avatar ${editable ? 'scale_trns cur_pointer' : ''}`}
+        style={style}
+        onClick={() => editable && setEdit(true)}
+      >
+        <AvatarImgLetter letter={letter} img={img} />
       </div>
+      {edit && (
+        <AvatarEditAlert
+          onHide={() => setEdit(false)}
+          letter={letter}
+          img={img}
+        />
+      )}
+    </>
+  )
+}
+
+function AvatarImgLetter({ letter, img }) {
+  return (
+    <>
+      {img && <img className="avatar_img" alt="Avatar" src={img} />}
+      {!img && letter && <div className="avatar_letter">{letter}</div>}
+      {!img && !letter && (
+        <span
+          className="material-symbols-outlined avatar_letter"
+          style={{ fontSize: 'inherit' }}
+        >
+          sentiment_dissatisfied
+        </span>
+      )}
     </>
   )
 }
---
+++ src/components/Avatar/AvatarEditAlert.jsx
@@ -0,0 +1,157 @@
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
+
+import Avatar from './Avatar'
+import Button from '../Button/Button'
+
+import { uploadAvatar } from '../../modules/avatar.module'
+
+import '../Alert/Alert.css'
+
+const AvatarEditAlertContext = createContext()
+
+export default function AvatarEditAlert({ onHide, letter, img: iImg }) {
+  const [imgs, setImgs] = useState({
+    img: iImg || '',
+    iImg: iImg || '',
+    imgFile: iImg || false,
+  })
+  const [alertDisabled, setAlertDisabled] = useState(false)
+
+  function handleDrop(e) {
+    e.preventDefault()
+
+    const file = e.dataTransfer.files[0]
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader()
+      reader.onload = () => {
+        setImgs({ ...imgs, img: reader.result, imgFile: file })
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  return (
+    <>
+      <AvatarEditAlertContext.Provider
+        value={{ imgs, setImgs, alertDisabled, setAlertDisabled, onHide }}
+      >
+        <div
+          className="avatar_edit_alert alert_area pos_full_page d_f_ce"
+          onDrop={handleDrop}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          <div
+            className="pos_full_page alert_bg blur_theme_bg"
+            onClick={onHide}
+          ></div>
+          <div className="alert_con d_f_ai_ce list_y">
+            <Avatar
+              style={{ fontSize: '70px', height: '150px' }}
+              letter={letter}
+              img={imgs.img}
+            ></Avatar>
+            <UploadButton />
+            {imgs.img && <RemoveButton />}
+            <SaveButton />
+          </div>
+        </div>
+      </AvatarEditAlertContext.Provider>
+    </>
+  )
+}
+
+function UploadButton() {
+  const inputRef = useRef()
+  const { imgs, setImgs, alertDisabled } = useContext(AvatarEditAlertContext)
+
+  function upload(e) {
+    const file = e.target.files[0]
+    if (!file) return
+
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      setImgs({ ...imgs, img: reader.result, imgFile: file })
+    }
+    reader.readAsDataURL(file)
+  }
+
+  return (
+    <>
+      <Button
+        className="w_100 btn_bd_cl txt_cl bg_h_none"
+        onClick={() => inputRef.current.click()}
+        disabled={alertDisabled}
+      >
+        Upload
+      </Button>
+      <input
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+        className="d_n"
+        onChange={upload}
+      />
+    </>
+  )
+}
+
+function RemoveButton() {
+  const { imgs, setImgs, alertDisabled } = useContext(AvatarEditAlertContext)
+
+  return (
+    <>
+      <Button
+        className="w_100 btn_bd_cr txt_red bg_h_none"
+        onClick={() => setImgs({ ...imgs, img: '', imgFile: false })}
+        disabled={alertDisabled}
+      >
+        Remove
+      </Button>
+    </>
+  )
+}
+
+const SAVE_BTN_TEXTS = {
+  save: 'Save',
+  saving: 'Saving',
+}
+function SaveButton() {
+  const { imgs, setAlertDisabled, onHide } = useContext(AvatarEditAlertContext)
+  const [disabled, setDisabled] = useState(imgs.img === imgs.iImg)
+  const [saveBtnText, setSaveBtnText] = useState(SAVE_BTN_TEXTS.save)
+
+  useEffect(() => {
+    setDisabled(imgs.img === imgs.iImg || saveBtnText === SAVE_BTN_TEXTS.saving)
+  }, [imgs.img, imgs.iImg, saveBtnText])
+
+  async function save() {
+    setAlertDisabled(true)
+    setSaveBtnText(SAVE_BTN_TEXTS.saving)
+
+    const uploaded = await uploadAvatar(imgs.imgFile)
+    if (uploaded) {
+      toast.success('Avatar saved!')
+      onHide()
+    }
+    if (!uploaded) toast.error('Failed to save avatar')
+
+    setSaveBtnText(SAVE_BTN_TEXTS.save)
+    setAlertDisabled(false)
+  }
+
+  return (
+    <>
+      <div
+        className={`con w_100 txt_ce blur_theme_bg blur_ha fz_medium pd_tb_small ${
+          !disabled ? 'scale_trns cur_pointer' : ''
+        }`}
+        onClick={save}
+        disabled={disabled}
+      >
+        {saveBtnText}
+      </div>
+    </>
+  )
+}
