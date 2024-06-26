84b8de0ce1f6b76623302460760becc62d4c13f9
Akbar <akbarjonjorayev001@gmail.com>
2024-06-26 20:07:49 +0500

    @ added for users

--- src/pages/GamesPage/components/GamePageWaitingForRes.jsx
+++ src/pages/GamesPage/components/GamePageWaitingForRes.jsx
@@ -46,7 +46,7 @@ export default function GamePageWaitingForRes({ name, link, onHide }) {
     <Alert onHide={stopGame}>
       <div className="list_y">
         <div className="txt_ce">
-          You're waiting for <b>{name}</b> response for{' '}
+          You're waiting for <b>@{name}</b> response for{' '}
           <b>{secondsTo(count)}</b>
         </div>
         <Button className="btn_bd txt_red" onClick={stopGame}>
