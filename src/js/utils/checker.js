6cebb4544312eca3bc9e2f915545b72b47ad0716
Akbar <akbarjonjorayev001@gmail.com>
2024-08-30 20:48:00 +0500

    google ads fixed

--- src/js/utils/checker.js
+++ src/js/utils/checker.js
@@ -5,7 +5,6 @@ import {
 
 export function checkRunApp() {
   checkInitialLocalStorage()
-  checkAds()
 }
 
 export function checkInitialLocalStorage() {
@@ -23,19 +22,4 @@ export function getInitialLocalStorage() {
   }
 
   return data
-}
-
-function checkAds() {
-  const script = document.createElement('script')
-  script.async = true
-  script.src =
-    'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7347223343628043'
-  script.crossOrigin = 'anonymous'
-
-  const meta = document.createElement('meta')
-  meta.name = 'google-adsense-account'
-  meta.content = 'ca-pub-7347223343628043'
-
-  document.head.appendChild(script)
-  document.head.appendChild(meta)
 }
